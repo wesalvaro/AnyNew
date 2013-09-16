252ee2bf6a7147a49fde73bfa0f595f58d528c13
Wes Alvaro <hello@wesalvaro.com>
2013-09-16 03:37:30 -0700

    Adds marking tasks as done and some comments.

--- any.js
+++ any.js
@@ -9,21 +9,31 @@ TODO: Fix animation?
 var any = angular.module('any', []);
 any.SERVER = 'https://sm-prod.any.do'
 any.LOGIN_URL = any.SERVER + '/j_spring_security_check';
-any.TASK_URL = any.SERVER + '/me/tasks';
+any.TASKS_URL = any.SERVER + '/me/tasks';
+any.TASK_URL = any.SERVER + '/me/tasks/';
 any.TASK_PARAMS = '?responseType=flat&includeDeleted=false&includeDone=false';
 
 
 
+/**
+ * Controls listing and marking tasks.
+ */
 any.TasksCtrl = function($scope, Any) {
   $scope.tasks = Any.getTasks();
   $scope.tasks.then(function() {
     $scope.loaded = true;
   });
+  $scope.markDone = function(gId) {
+    Any.markDone(gId);
+  };
 };
 any.controller('TasksCtrl', any.TasksCtrl);
 
 
 
+/**
+ * Controls login information.
+ */
 any.ConfigCtrl = function($scope, Config, Any) {
   $scope.config = Config;
   $scope.login = function() {
@@ -35,6 +45,9 @@ any.controller('ConfigCtrl', any.ConfigCtrl);
 
 
 
+/**
+ * Stores login information in the local store.
+ */
 any.Config = function() {
   this.email = localStorage.getItem('email');
   this.password = localStorage.getItem('password');
@@ -49,10 +62,15 @@ any.Config.prototype.save = function() {
 
 
 
+/**
+ * Interacts with the Any.Do API server.
+ */
 any.Any = function($http, $q, Config) {
   this.http = $http;
   this.q = $q;
   this.config = Config;
+  this.tasks = null;
+  this.tasksById = {};
 };
 any.service('Any', any.Any);
 
@@ -61,9 +79,9 @@ any.Any.prototype.login = function() {
   if (this.loggedIn) {
     return this.q.when(true);
   }
-  data = ('j_username=' + this.config.email +
-          '&j_password=' + this.config.password +
-          '&_spring_security_remember_me=on');
+  var data = ('j_username=' + this.config.email +
+              '&j_password=' + this.config.password +
+              '&_spring_security_remember_me=on');
   var self = this;
   return this.http({
     url: any.LOGIN_URL,
@@ -75,13 +93,36 @@ any.Any.prototype.login = function() {
     return true;
   });
 
-}
+};
+
+
+any.Any.prototype.markDone = function(globalTaskId, done) {
+  done = angular.isDefined(done) ? done : true;
+  var url = any.TASK_URL + globalTaskId + any.TASK_PARAMS;
+  var task = this.tasksById[globalTaskId];
+  task.status = done ? 'CHECKED' : 'UNCHECKED';
+  return this.http.put(url, task);
+};
+
+
+any.Any.prototype.refreshTasks = function() {
+  var url = any.TASKS_URL + any.TASK_PARAMS;
+  var self = this;
+  return this.tasks = this.http.get(url).then(function(data) {
+    angular.forEach(data.data, function(task) {
+      self.tasksById[task.globalTaskId] = task;
+    });
+    return data.data;
+  });
+};
 
 
 any.Any.prototype.getTasks = function() {
-  this.login();
-  var url = any.TASK_URL + any.TASK_PARAMS;
-  return this.http.get(url).then(function(data) {
-    return data.data;
+  if (this.tasks) {
+    return this.tasks;
+  }
+  var self = this;
+  return this.login().then(function() {
+    return self.refreshTasks();
   });
 };
